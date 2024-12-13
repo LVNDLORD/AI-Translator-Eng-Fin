804b545309aa1d2b336de89b5bfd537672f5e89e
andreydeshko <andydeshko@gmail.com>
2024-12-14 00:51:48 +0200

    updated ui to handle better an empty input

--- app.py
+++ app.py
@@ -5,21 +5,24 @@ from transformers import pipeline
 app = Flask(__name__)
 pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-en-fi")
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/translate', methods=['POST'])
 def translate():
     text = request.form.get('text')
     if not text:
         return jsonify({"error": "Text field is empty. Please enter your text."}), 400
-    
+
     try:
         translated_text = pipe(text, max_length=512)[0]['translation_text']
         return jsonify({"translated_text": translated_text})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+async function translateText() {
+    const text = document.getElementById('text').value;
+    const errorElement = document.getElementById('error');
+    const resultElement = document.getElementById('result');
+    const outputElement = document.getElementById('output');
+    outputElement.style.display = 'none'; 
+    resultElement.textContent = '';
+
+    errorElement.style.display = 'none';
+    errorElement.textContent = '';
+
+    if (text.trim() === '') {
+        errorElement.textContent = 'Text field is empty. Please enter your text.';
+        errorElement.style.display = 'block';
+        return;
+    }
+
+    // Fetch translation
+    try {
+        const response = await fetch('/translate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: `text=${encodeURIComponent(text)}`,
+        });
+
+        const data = await response.json();
+
+        // Update the translated_text or show an error
+        if (data.error) {
+            errorElement.textContent = data.error;
+            errorElement.style.display = 'block';
+            outputElement.style.display = 'none';
+        } else {
+            resultElement.textContent = data.translated_text;
+            outputElement.style.display = 'block';
+        }
+    } catch (error) {
+        errorElement.textContent = 'An unexpected error occurred. Please try again.';
+        errorElement.style.display = 'block';
+        outputElement.style.display = 'none';
+    }
+}
